8ab732df05bd6423e720da428f6be9635c0e66b0
ofemino <ofemino@gmail.com>
2022-11-20 19:19:43 +0100

    Updated IStudentRepository.cs

--- SchoolERP.Persistent/IRepositories/IStudentRepository.cs
+++ SchoolERP.Persistent/IRepositories/IStudentRepository.cs
@@ -1,6 +1,8 @@
+using SchoolERP.Domain.Dtos.Out;
+
 namespace SchoolERP.Persistent.IRepositories;
 
 public interface IStudentRepository
 {
-    
+    Task<List<StudentDtoOut>> GetStudents();
 }
--- SchoolERP.Services/StudentServices.cs
+++ SchoolERP.Services/StudentServices.cs
@@ -1,27 +1,45 @@
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
 using SchoolERP.Domain.Dtos;
 using SchoolERP.Domain.Dtos.Out;
+using SchoolERP.Persistent.IRepositories;
 using SchoolERP.Services.Interfaces;
 
 namespace SchoolERP.Services;
 
 public class StudentServices : IStudentServices
 {
+    private readonly ILogger<StudentServices> _logger;
+    private readonly IStudentRepository _repository;
     private ResponseObject<StudentDtoOut> _response;
+
+    public StudentServices(ILogger<StudentServices> logger, IStudentRepository repository)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
 
     public async Task<ResponseObject<StudentDtoOut>> GetStudents()
     {
         _response = new ResponseObject<StudentDtoOut>();
         try
         {
-            throw new NotImplementedException();
+            var students = await _repository.GetStudents();
+            if (students.Count > 0)
+            {
+                return new ResponseObject<StudentDtoOut>
+                    { ObjectResponseList = students, ResponseMessage = "success" };
+            }
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
+            _logger.LogError("Repo Error at GetStudents : {S}", ex.ToString());
+            return new ResponseObject<StudentDtoOut>
+                { ObjectResponseList = null, ResponseMessage = $"Error : {ex.Message}" };
         }
 
-        return _response;
+        return new ResponseObject<StudentDtoOut> { ObjectResponseList = null, ResponseMessage = "No record!" };
     }
 
     public async Task<ResponseObject<StudentDtoOut>> GetStudentById(long id)
